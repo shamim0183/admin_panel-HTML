dd452825c974fd6210c782f1e3514ab99cda490f
shamim <shaamim.hossaain@gmail.com>
2022-04-10 17:35:16 +0600

    html and css updated

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// ================== LEFT SIDEBAR JS ======================
+$(document).on('click', '#sidebar li', function(){
+    $(this).addClass('active').siblings().removeClass('active')
+});
+
+//============ LEFT MENU SIDEBAR DP TOGGLE ==============
+$('.sub-menu ul').hide();
+$(".sub-menu a").click(function(){
+    $(this).parent(".sub-menu").children("ul").slideToggle("100");
+    $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+});
+
+//============ SIDEBAR TOGGLE ============
+
+$(document).ready(function(){
+    $("#toggleSidebar").click(function(){
+        $(".left-menu").toggleClass("hide");
+        $(".content-wrapper").toggleClass("hide");
+    });
+});
